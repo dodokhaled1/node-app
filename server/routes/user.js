d46a8740ad8be4b49161f4ac5e80c31f589485b6
mohammad <mohammadalthabateh@gmail.com>
2022-09-30 00:36:01 +0300

    secound stage

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,13 +4,15 @@ const userControllers =  require('../controllers/userControllers')
 
 router.get('/', userControllers.view)
 router.post('/', userControllers.find)
-//router.get('/:id', userControllers.delete)
 
 router.get('/adduser', userControllers.form)
 router.post('/adduser', userControllers.create)
 router.get('/edituser/:id', userControllers.edit)
 router.post('/edituser/:id', userControllers.update)
 router.get('/viewuser/:id', userControllers.viewall)
+router.get('/:id', userControllers.delete)
+
+
 
 
 module.exports = router
